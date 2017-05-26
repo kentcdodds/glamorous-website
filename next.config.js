7408c1b89ce0683f0ea521f7c7c308579e13400d
Paul Molluzzo <paul@molluzzo.com>
2017-05-26 16:29:12 -0400

    Fix whitespace linting issue

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ module.exports = {
       test: /\.md$/,
       use: [{
         loader: 'html-loader'
-      },{
+      }, {
         loader: 'markdown-loader',
         options: {
           pedantic: true,
