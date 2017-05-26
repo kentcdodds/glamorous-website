fdfe77e2ff618c9abc40c1a86d70b3299ca0f68a
Paul Molluzzo <paul@molluzzo.com>
2017-05-26 16:30:45 -0400

    [WIP] - Add basic docs (#9)
    * Add in loaders for markdown support; update webpack config
    
    * Add in basic documentation loading from md file
    
    * Update snapshot for docs after adding MD file
    
    * Fix whitespace linting issue
    
    * Clean up md loaders in webpack config
    
    * Fix whitespace linting issue

--- pages/docs.js
+++ pages/docs.js
@@ -1,26 +1,52 @@
 import React from 'react'
 import {rehydrate} from 'glamor'
 import glamorous from 'glamorous'
-import Logo from '../components/glamorous-logo'
 import Layout from '../components/layout'
+import * as colors from '../styles/colors'
+
+// Define main file here so we can require the markdown file in the client
+let main = ''
 
 // Adds server generated styles to glamor cache.
 // Has to run before any `style()` calls
 // '__NEXT_DATA__.ids' is set in '_document.js'
 if (typeof window !== 'undefined' && window.__NEXT_DATA__ !== undefined) {
+  main = require('../docs/main.md')
   rehydrate(window.__NEXT_DATA__.ids)
 }
 
-const {Div} = glamorous
+const MarkdownWrapper = glamorous.div({
+  margin: '20px auto',
+  width: '100%',
+  maxWidth: '50rem',
 
-export default () => {
-  return (
-    <Layout>
-      <Div margin="20px auto" maxWidth={700} textAlign="center">
-        <Logo marginTop={50}/>
-        <glamorous.P>edit in pages/docs.js</glamorous.P>
-      </Div>
-    </Layout>
-  )
+  '& pre, & code': {
+    backgroundColor: '#eee',
+    fontSize: '1rem'
+  },
+  '& pre': {
+    padding: '1rem',
+    overflowX: 'scroll'
+  },
+  '& a': {
+    textDecoration: 'none',
+    color: colors.primary
+  }
+})
+
+export default class Docs extends React.Component {
+  rawMarkup() {
+    return {__html: main}
+  }
+
+  render() {
+    return (
+      <Layout>
+        <MarkdownWrapper>
+          <div dangerouslySetInnerHTML={this.rawMarkup()}/>
+        </MarkdownWrapper>
+      </Layout>
+    )
+  }
 }
 
