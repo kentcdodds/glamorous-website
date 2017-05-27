d9f5420bbce8e4770a60fb26beb9a432da41e8a9
Paul Molluzzo <paul@molluzzo.com>
2017-05-27 09:43:04 -0400

    DRY up rehydration by moving to layout

--- pages/docs.js
+++ pages/docs.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {rehydrate} from 'glamor'
 import glamorous from 'glamorous'
 import Layout from '../components/layout'
 import * as colors from '../styles/colors'
@@ -7,12 +6,9 @@ import * as colors from '../styles/colors'
 // Define main file here so we can require the markdown file in the client
 let main = ''
 
-// Adds server generated styles to glamor cache.
-// Has to run before any `style()` calls
-// '__NEXT_DATA__.ids' is set in '_document.js'
+// Adds in main.md on client only
 if (typeof window !== 'undefined' && window.__NEXT_DATA__ !== undefined) {
   main = require('../docs/main.md')
-  rehydrate(window.__NEXT_DATA__.ids)
 }
 
 const MarkdownWrapper = glamorous.div({
--- pages/guides.js
+++ pages/guides.js
@@ -1,15 +1,7 @@
 import React from 'react'
-import {rehydrate} from 'glamor'
 import glamorous from 'glamorous'
 import Logo from '../components/glamorous-logo'
 import Layout from '../components/layout'
-
-// Adds server generated styles to glamor cache.
-// Has to run before any `style()` calls
-// '__NEXT_DATA__.ids' is set in '_document.js'
-if (typeof window !== 'undefined' && window.__NEXT_DATA__ !== undefined) {
-  rehydrate(window.__NEXT_DATA__.ids)
-}
 
 const {Div} = glamorous
 
