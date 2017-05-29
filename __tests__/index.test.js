9bb9a58580e5c943cd20036c1499499827dc2dc9
Paul Molluzzo <paul@molluzzo.com>
2017-05-29 15:36:17 -0400

    [WIP] Use theme provider (#16)
    * feat: Add in ThemeProvider and base styles
    
    ThemeProvider was added in the layout component to pass global styles down for other components.
    Static global base styles were added and use glamor's `css` to get injected.
    
    * refactor: Remove glamor rehydrate method
    
    This removes the rehydrate method call since it seems to be optional and was reducing test coverage
    
    * fix(tests): Update test snapshots"
    
    * Revert "refactor: Remove glamor rehydrate method"
    
    This reverts commit a4734616e5082c6390012d503bcfbb7e45319e3b.
    
    * test: Add in jest-glamor-react

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,12 +2,16 @@
 
 import React from 'react'
 import renderer from 'react-test-renderer'
+import {matcher, serializer} from 'jest-glamor-react'
 import Index from '../pages/index'
+
+expect.addSnapshotSerializer(serializer)
+expect.extend(matcher)
 
 describe('Testing Index', () => {
   it('Renders!', () => {
     const component = renderer.create(<Index/>)
     const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(tree).toMatchSnapshotWithGlamor()
   })
 })
