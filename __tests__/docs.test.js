23f308a6f965af9777978a285389b49a6f51c063
Paul Molluzzo <paul@molluzzo.com>
2017-05-29 10:02:43 -0400

    test: Add in jest-glamor-react

--- __tests__/docs.test.js
+++ __tests__/docs.test.js
@@ -2,7 +2,11 @@
 
 import React from 'react'
 import renderer from 'react-test-renderer'
+import {matcher, serializer} from 'jest-glamor-react'
 import Docs from '../pages/docs'
+
+expect.addSnapshotSerializer(serializer)
+expect.extend(matcher)
 
 describe('Testing Docs', () => {
   it('Renders!', () => {
