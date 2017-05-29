a4734616e5082c6390012d503bcfbb7e45319e3b
Paul Molluzzo <paul@molluzzo.com>
2017-05-29 00:58:34 -0400

    refactor: Remove glamor rehydrate method
    This removes the rehydrate method call since it seems to be optional and was reducing test coverage

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,10 @@
 import React from 'react'
-import {css, rehydrate} from 'glamor'
+import {css} from 'glamor'
 import glamorous, {ThemeProvider} from 'glamorous'
 import baseStyles from '../styles/base'
 import GlobalStyles from '../styles/global-styles'
 import Nav from './nav'
 import Footer from './footer'
-
-// Adds server generated styles to glamor cache.
-// Has to run before any `style()` calls
-// '__NEXT_DATA__.ids' is set in '_document.js'
-if (typeof window !== 'undefined' && window.__NEXT_DATA__ !== undefined) {
-  rehydrate(window.__NEXT_DATA__.ids)
-}
 
 const Wrapper = glamorous.div((props, theme) => ({
   fontFamily: theme.font.glamorous
