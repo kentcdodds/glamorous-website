f55c7bd5c728ee2d6bbbf92df6846ba40c79f272
Paul Molluzzo <paul@molluzzo.com>
2017-05-31 23:32:20 -0400

    refactor(docs, examples, guides): Integrate hero on /examples, /guides, and /docs

--- pages/guides.js
+++ pages/guides.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import glamorous, {Div} from 'glamorous'
-import Logo from '../components/glamorous-logo'
 import Layout from '../components/layout'
+import Hero from '../components/hero'
 
 function Guides(props) {
   return (
     <Layout pathname={props.url ? props.url.pathname : ''}>
-      <Div margin="20px auto" maxWidth={700} textAlign="center">
-        <Logo marginTop={50} />
-        <glamorous.P>edit in pages/guides.js</glamorous.P>
-      </Div>
+      <Hero>
+        edit in pages/guides.js
+      </Hero>
     </Layout>
   )
 }
