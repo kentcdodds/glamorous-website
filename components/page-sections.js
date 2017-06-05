2523c36ae1984f3223abac3bc81e7104bc891d46
Paul Molluzzo <paul@molluzzo.com>
2017-06-04 21:43:53 -0400

    feat(page-sections): Add styles to page-sections; remove use of click-to-render

--- components/page-sections.js
+++ components/page-sections.js
@@ -1,37 +1,53 @@
 import React from 'react'
-import {Div} from 'glamorous'
+import glamorous, {Div} from 'glamorous'
 import Hero from './hero'
 import interactiveMarkdown from './interactive-markdown'
-import ClickToRender from './click-to-render'
 import CodeSandboxEmbed from './code-sandbox-embed'
+
+const PageWrapper = glamorous.div((props, {colors}) => ({
+  backgroundColor: colors.white,
+  width: '100%',
+  padding: '1rem',
+  '& > h3': {
+    width: '100%',
+    margin: '20px auto',
+    maxWidth: '50rem',
+  },
+}))
 
 export default PageSections
 
 function PageSections({data}) {
   return (
-    <Div margin="20px auto" maxWidth={700} textAlign="center">
+    <Div>
       <Hero>{data.title}</Hero>
-      {interactiveMarkdown(data.heading)}
-      <div>
+      <PageWrapper>
+        <h3>{interactiveMarkdown(data.heading)}</h3>
         {data.sections.map(section =>
           <DocSection key={section.title} {...section} />,
         )}
-      </div>
+      </PageWrapper>
     </Div>
   )
 }
 
 function DocSection(props) {
-  const {title, subtitle} = props
+  const {title} = props
+
+  // eslint-disable-next-line no-shadow
+  const Section = glamorous.section((props, {colors}) => ({
+    borderBottom: `1px solid ${colors.primary}`,
+    width: '100%',
+    margin: '20px auto',
+    paddingBottom: 20,
+    maxWidth: '50rem',
+  }))
+
   return (
-    <div>
+    <Section>
       <h2>{title}</h2>
-      <ClickToRender
-        summary={subtitle}
-        component={DocSectionDetails}
-        props={props}
-      />
-    </div>
+      <DocSectionDetails {...props} />
+    </Section>
   )
 }
 
