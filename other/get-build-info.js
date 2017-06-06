abd2ec3da3bf6d38fa15ed4ebe468b295fa884da
Kent C. Dodds <kent@doddsfamily.us>
2017-06-05 21:43:51 -0600

    WIP: improve get-build-info

--- other/get-build-info.js
+++ other/get-build-info.js
@@ -1,7 +1,17 @@
 const execSync = require('child_process').execSync
 
-const branch = exec('git rev-parse --abbrev-ref HEAD')
-const sha = exec('git rev-parse HEAD')
+const {
+  TRAVIS_BRANCH,
+  TRAVIS_PULL_REQUEST_BRANCH,
+  TRAVIS_PULL_REQUEST_SHA,
+  TRAVIS_COMMIT,
+} = process.env
+const branch =
+  TRAVIS_PULL_REQUEST_BRANCH ||
+  TRAVIS_BRANCH ||
+  exec('git rev-parse --abbrev-ref HEAD')
+const sha =
+  TRAVIS_PULL_REQUEST_SHA || TRAVIS_COMMIT || exec('git rev-parse HEAD')
 const commitDate = exec(`git show -s --format=%ci ${sha}`)
 const committer = exec(`git --no-pager show -s --format='%an' HEAD`)
 const buildInfo = {
