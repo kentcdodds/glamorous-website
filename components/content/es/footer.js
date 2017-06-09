3af69e9285c65b8cda0def5e724bc252c6a0cdaa
Kent C. Dodds <kent+github@doddsfamily.us>
2017-06-08 20:05:58 -0600

    New translations footer.js (Spanish)

--- components/content/es/footer.js
+++ components/content/es/footer.js
@@ -1,7 +1,7 @@
 module.exports = {
   madeWith: `
-    Hecho con <span role="img" aria-label="love">💙</span> por
-    <a href="https://github.com/paypal">PayPal</a> y
-    <a href="https://github.com/paypal/glamorous/blob/master/README.md#contributors">colaboradores</a>.
+    Made with <span role="img" aria-label="love">💙</span> by
+    <a href="https://github.com/paypal">PayPal</a> and
+    <a href="https://github.com/paypal/glamorous/blob/master/README.md#contributors">contributors</a>.
   `,
 }
