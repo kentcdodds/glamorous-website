7c2ca4379f58c60e7f578f19751bc9b4a01f56e0
Kent C. Dodds <kent+github@doddsfamily.us>
2017-06-09 21:45:18 -0600

    New Crowdin translations (#62)
    * New translations footer.js (Spanish)
    
    * New translations recompose.js (French)
    
    * New translations existing-css.js (Spanish)
    
    * New translations index.js (Spanish)
    
    * New translations refs.js (Spanish)
    
    * New translations polished.js (French)
    
    * New translations next.js (French)
    
    * New translations create-react-app.js (French)
    
    * New translations glamor.js (French)
    
    * New translations index.js (French)
    
    * New translations jest.js (French)
    
    * New translations size.js (Spanish)
    
    * New translations ssr.js (Spanish)
    
    * New translations core-concepts.js (Spanish)
    
    * New translations dynamic-styles.js (Spanish)
    
    * New translations getting-started.js (Spanish)
    
    * New translations index.js (Spanish)
    
    * New translations animation.js (Spanish)
    
    * New translations typescript.js (Spanish)
    
    * New translations theming.js (Spanish)
    
    * New translations glamorous-component-factory.js (Spanish)
    
    * New translations glamorous.js (Spanish)
    
    * New translations index.js (Spanish)
    
    * New translations index.js (French)
    
    * New translations index.js (French)
    
    * New translations refs.js (French)
    
    * New translations size.js (French)
    
    * New translations ssr.js (French)
    
    * New translations theming.js (French)
    
    * New translations index.js (French)
    
    * New translations existing-css.js (French)
    
    * New translations locale-chooser.js (French)
    
    * New translations page-sections.js (French)
    
    * New translations home-page-example.js (French)
    
    * New translations index.js (French)
    
    * New translations glamorous-component-factory.js (French)
    
    * New translations glamorous.js (French)
    
    * New translations index.js (French)
    
    * New translations install.js (French)
    
    * New translations react-native.js (French)
    
    * New translations css-grid.js (French)
    
    * New translations getting-started.js (French)
    
    * New translations dynamic-styles.js (French)
    
    * New translations index.js (French)
    
    * New translations typescript.js (French)
    
    * New translations animation.js (French)
    
    * New translations core-concepts.js (French)
    
    * New translations install.js (Spanish)
    
    * New translations react-native.js (Spanish)
    
    * New translations jest.js (Spanish)
    
    * New translations next.js (Spanish)
    
    * New translations polished.js (Spanish)
    
    * New translations recompose.js (Spanish)
    
    * New translations index.js (Spanish)
    
    * New translations glamor.js (Spanish)
    
    * New translations css-grid.js (Spanish)
    
    * New translations index.js (Spanish)
    
    * New translations index.js (Spanish)
    
    * New translations create-react-app.js (Spanish)
    
    * New translations footer.js (French)

--- components/content/fr/page-sections.js
+++ components/content/fr/page-sections.js
@@ -1,3 +1,3 @@
 module.exports = {
-  tryIt: 'Try It',
+  edit: 'Edit',
 }
---
+++ pages/advanced/content/es/existing-css.js
@@ -0,0 +1,6 @@
+module.exports = {
+  title: 'Working with existing CSS',
+  subtitle: '',
+  description: `testing`.replace(/~/g, '`'),
+  filename: __filename,
+}
