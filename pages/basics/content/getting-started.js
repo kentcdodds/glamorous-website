7f84cd40c29d97592d13422be7a710ef7b404714
liadbiz <2712079998@qq.com>
2017-06-11 17:20:07 +0800

    docs($browser): add get-started section to doc(basics)
    move get-started part of README file in glamorous repo to glamorous website
    
    82

--- pages/basics/content/getting-started.js
+++ pages/basics/content/getting-started.js
@@ -1,6 +1,31 @@
 module.exports = {
   title: 'Getting Started',
-  subtitle: '',
-  description: ``.replace(/~/g, '`'),
+  subtitle: 'start using Glamorous',
+  description: `
+  <a href="https://youtu.be/lmrQTpJ_3PM" title="glamorous walkthrough">
+    <img src="https://github.com/paypal/glamorous/raw/master/other/glamorous-walkthrough.png" alt="Video Screenshot" title="Video Screenshot" width="700" />
+  </a>
+
+  Here are a basic example that show you how to get started making component with ~css-in-js~ magic
+
+  ~~~interactive
+  const { Div } = glamorous
+
+  function App() {
+    return (
+      <Div
+        fontSize={20}
+        textAlign="center"
+      >
+        Hello Glamorous!
+      </Div>
+    )
+  }
+
+  render(<App/>)
+  ~~~
+
+  `.replace(/~/g, '`'),
+  codesandboxId: 'mDLZ1oKn',
   filename: __filename,
 }
