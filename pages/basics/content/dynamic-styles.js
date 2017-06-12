ecf989fff2511e2b805d6c72c50168af5c49ec6b
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-06-12 05:39:24 -0700

    docs($browser): add dynamic styles info to docs (basics section) (#86)
    transferred the dynamic styles + static styles section from the glamorous github README to the
    glamorous website
    
    #83

--- pages/basics/content/dynamic-styles.js
+++ pages/basics/content/dynamic-styles.js
@@ -1,6 +1,87 @@
 module.exports = {
-  title: 'Dynamic Styles',
+  title: 'Dynamic Styles + Static Styles',
   subtitle: '',
-  description: ``.replace(/~/g, '`'),
+  description: `
+  One of the nice bits of glamorous is that it allows you to make a clear
+  separation between your dynamic and static styles by forcing you to choose
+  between an object literal and a function. Here's an example of having both
+  dynamic and static styles:
+
+  ~~~js
+  const MyLink = glamorous.a(
+    {
+      color: 'blue',
+      textDecoration: 'none',
+    },
+    ({size = 'small'}) => ({
+      fontSize: size === 'big' ? 24 : 16,
+    }),
+    // you can continue to provide any number of arguments
+    // and ~glamor~ will merge them. In the event of a
+    // style conflict, the last one wins.
+  )
+  ~~~
+
+  You can see a live preview of this example on [codesandbox](https://codesandbox.io/s/mZkpo0lKA).
+
+  <details>
+  <summary>Note, you can also use arrays of styles if you need:</summary>
+
+  ~~~js
+  const MyDiv = glamorous.div(
+    [
+      {
+        [phoneMediaQuery]: {
+          lineHeight: 1.2,
+        },
+      },
+      {
+        [phoneMediaQuery]: {
+          lineHeight: 1.3, // this will win because it comes later
+        },
+      },
+    ],
+    ({big, square}) => {
+      const bigStyles = big ?
+      {
+        [phoneMediaQuery]: {
+          fontSize: 20,
+        },
+      } :
+        {}
+
+      const squareStyles = square ?
+      {
+        [phoneMediaQuery]: {
+          borderRadius: 0,
+        },
+      } :
+      {
+        [phoneMediaQuery]: {
+          borderRadius: '50%',
+        },
+      }
+      // note that I'm returning an array here
+      return [bigStyles, squareStyles]
+    },
+  )
+
+  // result of <MyDiv big={true} square={false} /> will be:
+  // @media (max-width: 640px) {
+  //   .css-1bzhvkr,
+  //   [data-css-1bzhvkr] {
+  //     line-height: 1.3;
+  //     font-size: 20px;
+  //     border-radius: 50%;
+  //   }
+  // }
+  //
+  // <div
+  //   class="css-1bzhvkr"
+  // />
+  ~~~
+
+  </details>
+  `.replace(/~/g, '`'),
   filename: __filename,
 }
