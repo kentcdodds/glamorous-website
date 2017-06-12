dbb5df076070e75131435a711360609200c987c7
Kent C. Dodds <kent+github@doddsfamily.us>
2017-06-12 06:45:30 -0600

    docs(reactNative): indent

--- pages/basics/content/react-native.js
+++ pages/basics/content/react-native.js
@@ -2,13 +2,13 @@ module.exports = {
   title: 'glamorous-native',
   subtitle: 'React Native',
   description: `
-  ~glamorous~ offers a version for React Native projects called ~glamorous-native~.
+    ~glamorous~ offers a version for React Native projects called ~glamorous-native~.
 
-  ~~~js
-  npm install glamorous-native --save
-  ~~~
+    ~~~js
+    npm install glamorous-native --save
+    ~~~
 
-  You can learn more at the [glamorous-native project](https://github.com/robinpowered/glamorous-native).
+    You can learn more at the [glamorous-native project](https://github.com/robinpowered/glamorous-native).
   `.replace(/~/g, '`'),
   filename: __filename,
 }
