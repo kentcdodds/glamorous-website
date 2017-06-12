104b895f3e7d00b0af76890a85dfd377f2f9b960
Audora Chalker <scribeklio@gmail.com>
2017-06-12 08:48:10 -0400

    docs(CRA): add CRA integration docs (#93)
    * Create create-react-app.js
    
    #68
    What: copied create-react-app from glamorous repo to glamorous website
    
    Why: Adding existing glamorous docs to glamorous site. 
    
    How: copied content from glamorous repo README to /content/create-react-app.js
    
    * Update create-react-app.js
    
    * docs(CRA): indent and slightly modify

--- pages/integrations/content/create-react-app.js
+++ pages/integrations/content/create-react-app.js
@@ -1,3 +1,8 @@
 module.exports = `
-# [~create-react-app~](https://github.com/facebookincubator/create-react-app)
-`.replace(/~/g, '`')
+  title: '[`create-react-app`](https://github.com/facebookincubator/create-react-app)', 
+  description: `
+    [~Here~](https://github.com/patitonar/create-react-app-glamorous) is an example of
+     using ~glamorous~ with ~create-react-app~.
+  `.replace(/~/g, '`'), 
+  filename: __filename,
+}
