4981305594f9c6295766ca8baf1257e1cc337dae
Kent C. Dodds <kent@doddsfamily.us>
2017-06-12 07:10:24 -0600

    fix: synatx 😅

--- pages/integrations/content/create-react-app.js
+++ pages/integrations/content/create-react-app.js
@@ -1,8 +1,9 @@
-module.exports = `
-  title: '[`create-react-app`](https://github.com/facebookincubator/create-react-app)', 
+module.exports = {
+  title:
+    '[`create-react-app`](https://github.com/facebookincubator/create-react-app)',
   description: `
     [~Here~](https://github.com/patitonar/create-react-app-glamorous) is an example of
      using ~glamorous~ with ~create-react-app~.
-  `.replace(/~/g, '`'), 
+  `.replace(/~/g, '`'),
   filename: __filename,
 }
