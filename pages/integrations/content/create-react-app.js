56f4bc14fa129e92ab0ff408a151de9bf5d48012
Kent C. Dodds <kent+github@doddsfamily.us>
2017-06-12 06:47:43 -0600

    docs(CRA): indent and slightly modify

--- pages/integrations/content/create-react-app.js
+++ pages/integrations/content/create-react-app.js
@@ -1,8 +1,8 @@
 module.exports = `
-title: '`with [~create-react-app~](https://github.com/facebookincubator/create-react-app)`', 
-subtitle: '', 
-description: `
- [~Here~] (https://github.com/patitonar/create-react-app-glamorous) is an example of using ~glamorous~ with ~create-react-app~.
-`.replace(/~/g, '`'), 
-filename: __filename,
+  title: '[`create-react-app`](https://github.com/facebookincubator/create-react-app)', 
+  description: `
+    [~Here~](https://github.com/patitonar/create-react-app-glamorous) is an example of
+     using ~glamorous~ with ~create-react-app~.
+  `.replace(/~/g, '`'), 
+  filename: __filename,
 }
