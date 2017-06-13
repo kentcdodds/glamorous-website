be31c9179b806868d8a5e71014d68ea7b2d1803d
Aaron Olson <aarondolson@users.noreply.github.com>
2017-06-12 22:27:51 -0500

    Create jest.js
    Adding jest example from the glamorous repo to the website

--- pages/integrations/content/jest.js
+++ pages/integrations/content/jest.js
@@ -1,3 +1,9 @@
-module.exports = `
-# [~jest~](http://facebook.github.io/jest/)
-`.replace(/~/g, '`')
+module.exports = {
+  title: 
+    '[`jest`](https://facebook.github.io/jest/)',
+  description: `
+    [~Here~](https://github.com/paypal/glamorous/tree/master/examples/with-jest) is an example of
+     using ~glamorous~ with ~jest~.
+  `.replace(/~/g, '`'),
+  filename: __filename,
+}
