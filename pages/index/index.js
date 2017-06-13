c952a0ea02fe4741f68d23e96714c745178834de
William <wcastand@gmail.com>
2017-06-13 19:34:20 +0200

    Reduce size of the code live editor and adapt size to fit in a small screen (styled-component) (#117)
    * made the live code example smaller, reduce logo size to fit the code in a macbook 13" and delete paddingTop on code container to make the centering of element even with logo, title and button
    
    * create a custom code editor for the homepage, and revert change on components/code-preview.js
    
    * update a broken link from copy/paste

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import glamorous from 'glamorous'
+import {LiveProvider, LiveEditor, LiveError, LivePreview} from 'react-live'
+import { LiveContextTypes } from 'react-live/lib/components/Live/LiveProvider'
+import stripIndent from '../../components/utils/strip-indent'
 import {withContent} from '../../components/locale'
 import Layout from '../../components/layout'
 import {Button} from '../../components/styled-links'
-import CodePreview from '../../components/code-preview'
 import Hero from '../../components/hero'
 import getHomePageExample from '../../examples/home-page-example'
 
 const CodePreviewWrapper = glamorous.div((props, {colors}) => ({
   position: 'relative',
   padding: '1em',
+  paddingTop: 0,
   background: 'transparent',
   '::after': {
     content: '""',
@@ -51,6 +54,62 @@ const GettingStarted = glamorous(Button)(
     color: colors.white,
   }),
 )
+
+const StyledLiveProvider = glamorous(LiveProvider)({
+  margin: '0 auto',
+  display: 'flex',
+  flexDirection: 'column',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around',
+  maxWidth: '50rem',
+})
+
+const StyledLiveEditor = glamorous(LiveEditor)({})
+
+const StyledLiveError = glamorous(LiveError)((props, {colors, fonts}) => ({
+  color: colors.code,
+  fontFamily: fonts.monospace,
+  backgroundColor: colors.white,
+  flexBasis: '100%',
+  width: '100%',
+  maxWidth: '100%',
+  padding: '1rem',
+}))
+
+
+const HomepageLivePreview = ({ className, ...rest }, { live: { element: Button }, live }) => {
+  return (
+    <glamorous.Div textAlign="center" marginBottom="30px">
+      <Button href="https://github.com/paypal/glamorous" primary>
+        GitHub
+      </Button>
+      <Button href="http://kcd.im/glamorous-help">
+        Try it
+      </Button>
+    </glamorous.Div>
+  )
+}
+
+HomepageLivePreview.contextTypes = LiveContextTypes
+
+
+const StyledLivePreview = glamorous(HomepageLivePreview)({
+  padding: '1rem',
+})
+
+
+function CodePreview({noInline = true, code, scope = {glamorous}}) {
+  return (
+    <StyledLiveProvider
+      code={stripIndent(code).trim()}
+      scope={scope}
+    >
+      <StyledLivePreview />
+      <StyledLiveError />
+      <StyledLiveEditor />
+    </StyledLiveProvider>
+  )
+}
 
 function Home({url, content, locale}) {
   return (
