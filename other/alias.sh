f2de5a42b7926f067c3efb8f8eb741c5dee22e75
Kent C. Dodds <kent@doddsfamily.us>
2017-06-13 15:09:05 -0600

    fix build and stuff

--- other/alias.sh
+++ other/alias.sh
@@ -4,11 +4,7 @@ aliasDeployment() {
   echo "now alias set $NOW_ID $1$NOW_ALIAS --token NOW_TOKEN"
   now alias set $NOW_ID $1$NOW_ALIAS --token $NOW_TOKEN
 }
-echo "⏰ Waiting for $NOW_ID deployment to be ready"
-await-url $NOW_ID
-echo "🤘 $NOW_ID is ready, beginning the alias"
 aliasDeployment
 aliasDeployment en.
 aliasDeployment es.
 aliasDeployment fr.
-echo "🎊 Aliasing is finished!"
