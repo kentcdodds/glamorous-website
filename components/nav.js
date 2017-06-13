4bc565a4ad3257d359c99f4cc8469b82d5cf38bc
Paul Molluzzo <paul@molluzzo.com>
2017-06-13 09:19:57 -0400

    feat(nav): Add mobile styles for navigation (#108)
    * feat(nav): Add mobile styles for navigation
    
    #26, closes #106
    
    * fix(links) Turn active link styles back on

--- components/nav.js
+++ components/nav.js
@@ -2,26 +2,24 @@ import React from 'react'
 import glamorous from 'glamorous'
 import {Anchor} from '../components/styled-links'
 import LipstickIcon from './lipstick-icon'
+import Separator from './separator'
+import LocaleChooser from './locale-chooser'
+import MenuSVG from './svgs/menu.svg'
 
-const ListItem = glamorous.li({
-  paddingLeft: 10,
-  paddingRight: 10,
-  paddingBottom: 4,
-})
-
-// Use withTheme with glamorous.Ul, or this ?
-const List = glamorous.ul((props, {mediaQueries}) => ({
-  listStyle: 'none',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontSize: '1.25em',
-  margin: '0 auto',
-  padding: 0,
-  maxWidth: '50rem',
-  height: '4rem',
+const NavToggle = glamorous.a((props, {colors, mediaQueries}) => ({
+  fill: colors.primaryMed,
+  backgroundColor: colors.white,
+  display: 'block',
+  width: '100%',
   [mediaQueries.mediumUp]: {
-    justifyContent: 'flex-end',
+    display: 'none',
+  },
+}))
+
+const NavSeparator = glamorous(Separator)((props, {mediaQueries}) => ({
+  height: 1,
+  [mediaQueries.mediumUp]: {
+    display: 'none',
   },
 }))
 
@@ -35,68 +33,114 @@ const Hidden = glamorous.span({
   overflow: 'hidden',
 })
 
-export default Nav
+const ListItem = glamorous.li({
+  paddingLeft: 10,
+  paddingRight: 10,
+  paddingBottom: 4,
+})
 
-function Nav({pathname}) {
-  return (
-    <glamorous.Nav>
-      <List>
-        <ListItem>
-          <Anchor
-            prefetch={process.env.USE_PREFETCH}
-            href="/"
-            pathname={pathname}
-          >
-            <LipstickIcon width={20} />
-            <Hidden>Home</Hidden>
-          </Anchor>
-        </ListItem>
-        <ListItem>
-          <Anchor
-            prefetch={process.env.USE_PREFETCH}
-            href="/basics"
-            pathname={pathname}
-          >
-            Basics
-          </Anchor>
-        </ListItem>
-        <ListItem>
-          <Anchor
-            prefetch={process.env.USE_PREFETCH}
-            href="/advanced"
-            pathname={pathname}
-          >
-            Advanced
-          </Anchor>
-        </ListItem>
-        <ListItem>
-          <Anchor
-            prefetch={process.env.USE_PREFETCH}
-            href="/examples"
-            pathname={pathname}
-          >
-            Examples
-          </Anchor>
-        </ListItem>
-        <ListItem>
-          <Anchor
-            prefetch={process.env.USE_PREFETCH}
-            href="/integrations"
-            pathname={pathname}
-          >
-            Integrations
-          </Anchor>
-        </ListItem>
-        <ListItem>
-          <Anchor
-            prefetch={process.env.USE_PREFETCH}
-            href="/api"
-            pathname={pathname}
-          >
-            API
-          </Anchor>
-        </ListItem>
-      </List>
-    </glamorous.Nav>
-  )
+// Use withTheme with glamorous.Ul, or this ?
+const List = glamorous.ul((props, {colors, mediaQueries}) => ({
+  listStyle: 'none',
+  display: 'block',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '1.25em',
+  margin: '0 auto',
+  padding: 0,
+  maxWidth: '50rem',
+  height: 'auto',
+  width: '100%',
+  maxHeight: props.isOpen ? '100%' : '0',
+  overflow: 'hidden',
+  textAlign: 'center',
+  backgroundColor: colors.white,
+  [mediaQueries.mediumUp]: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: 'auto',
+    maxHeight: '4rem',
+    backgroundColor: 'inherit',
+  },
+}))
+
+export default class Nav extends React.Component {
+  state = {
+    open: false,
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {open: !prevState.open}
+    })
+  }
+
+  render() {
+    return (
+      <glamorous.Nav width="100%">
+        <NavToggle onClick={this.handleClick.bind(this)}><MenuSVG /></NavToggle>
+        <NavSeparator />
+        <List isOpen={this.state.open}>
+          <ListItem>
+            <LocaleChooser locale={this.props.locale} />
+          </ListItem>
+          <ListItem>
+            <Anchor
+              prefetch={process.env.USE_PREFETCH}
+              href="/"
+              pathname={this.props.pathname}
+            >
+              <LipstickIcon width={20} />
+              <Hidden>Home</Hidden>
+            </Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor
+              prefetch={process.env.USE_PREFETCH}
+              href="/basics"
+              pathname={this.props.pathname}
+            >
+              Basics
+            </Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor
+              prefetch={process.env.USE_PREFETCH}
+              href="/advanced"
+              pathname={this.props.pathname}
+            >
+              Advanced
+            </Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor
+              prefetch={process.env.USE_PREFETCH}
+              href="/examples"
+              pathname={this.props.pathname}
+            >
+              Examples
+            </Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor
+              prefetch={process.env.USE_PREFETCH}
+              href="/integrations"
+              pathname={this.props.pathname}
+            >
+              Integrations
+            </Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor
+              prefetch={process.env.USE_PREFETCH}
+              href="/api"
+              pathname={this.props.pathname}
+            >
+              API
+            </Anchor>
+          </ListItem>
+        </List>
+      </glamorous.Nav>
+    )
+  }
 }
