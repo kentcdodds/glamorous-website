e16418b46eff67b0b10209bf09ad8b952b1d5a8d
Kent C. Dodds <kent+github@doddsfamily.us>
2017-06-14 13:35:39 -0600

    minor tweak

--- pages/advanced/content/ssr.js
+++ pages/advanced/content/ssr.js
@@ -1,10 +1,10 @@
 module.exports = {
   title: 'Server Side Rendering',
-  subtitle: '',
-  description: `Because both ~glamor~ and ~react~ support SSR, glamorous does too! I actually do this 
-  on [my personal site](https://github.com/kentcdodds/kentcdodds.com) which is generated at build-time 
-  on the server. Learn about rendering [~react~ on the server](react on the server) and [~glamor~ too]
-  (https://github.com/threepointone/glamor/blob/5e7d988211330b8e2fca5bb8da78e35051444efd/docs/server.md).
+  description: `
+    Because both ~glamor~ and ~react~ support SSR, glamorous does too! I actually do this 
+    on [my personal site](https://github.com/kentcdodds/kentcdodds.com) which is generated at build-time 
+    on the server. Learn about rendering [~react~ on the server](react on the server) and [~glamor~ too]
+    (https://github.com/threepointone/glamor/blob/5e7d988211330b8e2fca5bb8da78e35051444efd/docs/server.md).
   `.replace(/~/g, '`'),
   filename: __filename,
 }
