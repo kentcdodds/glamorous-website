4bad7aca947621ce2e02534142c6cec50895432d
Kent C. Dodds <kent@doddsfamily.us>
2017-06-13 21:57:03 -0600

    add pseudo component example

---
+++ pages/integrations/content/pseudo.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '[`glamorous-pseudo`](https://github.com/tkh44/glamorous-pseudo)',
+  subtitle: 'A handy abstraction',
+  description: `
+    With the built-in components, if you want to use pseudo-states, you have to
+    use the ~css~ prop. If you'd rather not do that, then you can use
+    [~glamorous-pseudo~](https://github.com/tkh44/glamorous-pseudo).
+
+    ~~~callout {type: 'info', title: 'Inspiration'}
+    This was inspired by [this tweet](https://twitter.com/tkh44/status/873248130472562688)
+    from [@tkh44](https://twitter.com/tkh44). Thanks!
+    ~~~
+  `.replace(/~/g, '`'),
+  codeSandboxId: 'kj9xLJWN',
+  codeSandboxSummary: 'glamorous-pseudo demo',
+  filename: __filename,
+}
