b45393a216f1319291e8e3b4e4f52780e1206ecd
Tre Ammatuna <tre@tretuna.com>
2017-06-14 23:36:40 -0400

    docs(): add polished docs (integrations section) #67

--- pages/integrations/content/polished.js
+++ pages/integrations/content/polished.js
@@ -1,3 +1,30 @@
-module.exports = `
-# [~polished~](https://polished.js.org/)
-`.replace(/~/g, '`')
+module.exports = {
+  title: '[`✨ polished`](https://polished.js.org/)',
+  subtitle: '',
+  description: `
+    ~glamorous~ works with ~✨ polished~ mixins, helpers, and shorthands:
+    ~~~js
+    const MyStyledParagraph = glamorous.p({
+      fontSize: 20,
+      color: lighten(0.5, '#000'),
+    })
+    ~~~
+    You can also use object spread properties to apply more complex ✨ polished mixins directly onto glamorous components:
+    ~~~js
+    function GlamorousLogo() {
+      return (
+        <glamorous.Div
+          width={400}
+          height={400}
+          border="2px solid"
+          borderColor={mix(0.5, '#fff', '#000')}
+          {...borderRadius('top', '5px')}
+        >
+        </glamorous.Div>
+      );
+    }
+    ~~~
+     You can play more with ~✨ polished~ and ~glamorous~ [here](https://codesandbox.io/s/9Qo9kMgRZ).
+  `.replace(/~/g, '`'),
+  filename: __filename,
+}
