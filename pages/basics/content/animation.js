b9f2db5ab5e87f9ab9071c306fe9473c96240f1c
Tre Ammatuna <tre@tretuna.com>
2017-06-15 08:13:32 -0400

    docs(): add anitmation docs (basics section) #85

--- pages/basics/content/animation.js
+++ pages/basics/content/animation.js
@@ -1,6 +1,29 @@
 module.exports = {
   title: 'Animation',
   subtitle: '',
-  description: ``.replace(/~/g, '`'),
+  description: `
+    Try this in [your browser](https://codesandbox.io/s/31VMyP7XO)
+    ~~~js
+    // import css from glamor
+    import { css } from 'glamor'
+
+    // Define the animation styles
+    const animationStyles = props => {
+      const bounce = css.keyframes({
+        '0%': { transform: ~scale(1.01)~ },
+        '100%': { transform: ~scale(0.99)~ }
+      })
+      return {animation: ~\${bounce} 0.2s infinite ease-in-out alternate~}
+    }
+
+    // Define the element
+    const AnimatedDiv = glamorous.div(animationStyles)
+
+    // Use in a render function
+    <AnimatedDiv>
+      Bounce.
+    </AnimatedDiv>
+    ~~~
+  `.replace(/~/g, '`'),
   filename: __filename,
 }
