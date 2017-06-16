45db1671de21a90d10de42a8c11ffab3dc480415
aranajhonny <jhonnyjosearana@gmail.com>
2017-06-15 20:56:26 -0430

    Removes an extra word

--- components/content/es/footer.js
+++ components/content/es/footer.js
@@ -1,7 +1,7 @@
 module.exports = {
   madeWith: `
     Hecho con <span role="img" aria-label="love">💙</span> por
-    <a href="https://github.com/paypal">PayPal</a> et
+    <a href="https://github.com/paypal">PayPal</a> y
     <a href="https://github.com/paypal/glamorous/blob/master/README.md#contributors">los contribuidores</a>.
   `,
 }
