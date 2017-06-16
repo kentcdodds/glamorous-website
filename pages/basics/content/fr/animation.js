25d60eed2164b76e514e6b632cba1cfc63de2f2a
Forresst <forresst17@gmail.com>
2017-06-16 15:11:56 +0200

    docs(french): translation in french (last part) (#153)
    * docs(french): translation in french (last part)
    
    * Integrations
    * Updates from the original
    
    * Fix missed translation

--- pages/basics/content/fr/animation.js
+++ pages/basics/content/fr/animation.js
@@ -1,6 +1,34 @@
 module.exports = {
   title: 'Animation',
   subtitle: '',
-  description: ``.replace(/~/g, '`'),
+  description: `
+    Pour faire de l'animation avec glamorous, vous pouvez utiliser pour des choses simples
+    des transitions CSS ordinaires et pour des choses plus avancées, vous pouvez utiliser ~keyframes~
+    via l'API ~css.keyframes~ de glamour.
+
+    ~~~js
+    // importe css depuis glamor
+    import { css } from 'glamor'
+
+    // Définit les styles de l'animation
+    const animationStyles = props => {
+      const bounce = css.keyframes({
+        '0%': { transform: ~scale(1.01)~ },
+        '100%': { transform: ~scale(0.99)~ }
+      })
+      return {animation: ~\${bounce} 0.2s infinite ease-in-out alternate~}
+    }
+
+    // Definit l'élément
+    const AnimatedDiv = glamorous.div(animationStyles)
+
+    // Utilisation dans une fonction de rendu
+    <AnimatedDiv>
+      Bounce.
+    </AnimatedDiv>
+    ~~~
+  `.replace(/~/g, '`'),
+  codeSandboxId: '31VMyP7XO',
+  codeSandboxSummary: "exemple d'animation",
   filename: __filename,
 }
---
+++ pages/integrations/content/fr/polished.js
@@ -0,0 +1,34 @@
+module.exports = {
+  title: '[`✨ polished`](https://polished.js.org/)',
+  subtitle: '',
+  description: `
+    ~glamorous~ fonctionne avec les mixins, les helpers et les raccourcis de ~✨ polished~ :
+
+    ~~~js
+    const MyStyledParagraph = glamorous.p({
+      fontSize: 20,
+      color: lighten(0.5, '#000'),
+    })
+    ~~~
+
+    Vous pouvez aussi utiliser l'[opérateur de décomposition](https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/Op%C3%A9rateur_de_d%C3%A9composition) sur les propriétés d'un objet pour appliquer des mixins de ✨ polished plus complexes directement sur des composants glamorous :
+    
+    ~~~js
+    function GlamorousLogo() {
+      return (
+        <glamorous.Div
+          width={400}
+          height={400}
+          border="2px solid"
+          borderColor={mix(0.5, '#fff', '#000')}
+          {...borderRadius('top', '5px')}
+        >
+        </glamorous.Div>
+      );
+    }
+    ~~~
+  `.replace(/~/g, '`'),
+  codeSandboxId: '9Qo9kMgRZ',
+  codeSandboxSummary: 'démo polished + glamorous',
+  filename: __filename,
+}
