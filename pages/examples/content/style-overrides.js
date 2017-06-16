335876e1ee47706b008071cc082ede8aba068101
Kent C. Dodds <kent@doddsfamily.us>
2017-06-16 14:27:29 -0600

    Add style-overrides example

---
+++ pages/examples/content/style-overrides.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Accept style overrides',
+  subtitle: 'How to expose an API to override styles in a component',
+  description: `
+    This demonstrates one way that you could take a reusable component and expose
+    a mechanism for overriding styles for components within the component using
+    a prop called ~styleOverrides~.
+
+    The key bit here is passing ~styleOverrides~ to the ~theme~ prop of the
+    glamorous ~ThemeProvider~. Because you may still need to use the ~theme~
+    for other things, it's nice to namespace these (like this example does).
+
+    Then you can write a little helper function (~getStyleOverrides~) to make
+    adding this overrides capability to each of your glamorous components.
+    It even works with the ~css~ prop!
+  `.replace(/~/g, '`'),
+  codeSandboxId: 'ERNVNoxEv',
+  filename: __filename,
+}
