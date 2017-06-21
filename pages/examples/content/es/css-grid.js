f324981649d7a2a3530e459a785ef919816419e0
Arana Jhonny <jhonnyjosearana@gmail.com>
2017-06-21 14:38:20 -0400

    Update links to spanish mdn docs.

--- pages/examples/content/es/css-grid.js
+++ pages/examples/content/es/css-grid.js
@@ -2,11 +2,11 @@ module.exports = {
   title: 'CSS Grid',
   subtitle: 'La mejor solución para el diseño CSS',
   description: `
-    [CSS Grid Layout](https://developer.mozilla.org/fr/docs/Web/CSS/CSS_Grid_Layout)
+    [CSS Grid Layout](https://developer.mozilla.org/es/docs/Web/CSS/CSS_Grid_Layout)
     es una herramienta de diseño relativamente nueva para la web. Es increíblemente potente y expresiva,
     permite nuevos diseños que antes eran muy difíciles o totalmente imposibles.
 
-    En este ejemplo, usamos la función [~@supports~](https://developer.mozilla.org/fr/docs/Web/CSS/@supports)
+    En este ejemplo, usamos la función [~@supports~](https://developer.mozilla.org/es/docs/Web/CSS/@supports)
     de CSS para optar por CSS Grid en los navegadores donde está disponible. Ten en cuenta que
     no todos los navegadores soportarán ~@supports~.
   `.replace(/~/g, '`'),
