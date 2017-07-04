e8608ae758d71d9eea8b33afcaf309cd8e29873f
Kent C. Dodds <kent+github@doddsfamily.us>
2017-07-03 20:54:21 -0600

    adding the first german translations (#195)
    * adding the first german translations
    
    * update things

--- components/__tests__/locale-chooser.js
+++ components/__tests__/locale-chooser.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import {mount} from 'enzyme'
+import {render, mount} from 'enzyme'
 import toJson from 'enzyme-to-json'
 import {ThemeProvider} from 'glamorous'
 import {matcher, serializer} from 'jest-glamor-react'
 import GlobalStyles from '../../styles/global-styles'
 import LocaleChooser from '../locale-chooser'
 
+const {supportedLocales, fallbackLocale} = require('../../config.json')
+
 expect.addSnapshotSerializer(serializer)
 expect.extend(matcher)
 
 test('closed state', () => {
-  const wrapper = localeChooser()
+  const wrapper = renderLocaleChooser()
 
   const links = wrapper.find('a')
   expect(links.length).toBe(0)
@@ -19,28 +21,35 @@ test('closed state', () => {
 })
 
 test('opened state', () => {
-  const wrapper = localeChooser()
+  const wrapper = mountLocaleChooser()
   wrapper.find('button').simulate('click')
-
   const links = wrapper.find('a')
-  expect(links.length).toBe(4)
+  expect(links.length).toBe(supportedLocales.length + 1)
   expect(links.at(0).getDOMNode().getAttribute('href')).not.toEqual(
     expect.stringContaining('en.'),
   )
-  expect(links.at(1).getDOMNode().getAttribute('href')).toEqual(
-    expect.stringContaining('es.'),
-  )
-  expect(links.at(2).getDOMNode().getAttribute('href')).toEqual(
-    expect.stringContaining('fr.'),
-  )
-  expect(links.at(3).getDOMNode().getAttribute('href')).toEqual(
-    expect.stringContaining('CONTRIBUTING_DOCUMENTATION.md'),
-  )
-
-  expect(toJson(wrapper)).toMatchSnapshotWithGlamor()
+  supportedLocales.forEach((l, i) => {
+    if (l === fallbackLocale) {
+      return
+    }
+    expect(links.at(i).getDOMNode().getAttribute('href')).toEqual(
+      expect.stringContaining(`${l}.`),
+    )
+  })
+  expect(
+    links.at(supportedLocales.length).getDOMNode().getAttribute('href'),
+  ).toEqual(expect.stringContaining('CONTRIBUTING_DOCUMENTATION.md'))
 })
 
-function localeChooser() {
+function renderLocaleChooser() {
+  return render(
+    <ThemeProvider theme={GlobalStyles}>
+      <LocaleChooser />
+    </ThemeProvider>,
+  )
+}
+
+function mountLocaleChooser() {
   return mount(
     <ThemeProvider theme={GlobalStyles}>
       <LocaleChooser />
